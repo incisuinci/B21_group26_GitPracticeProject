f0e2d97e80b164b4c5f17c1116e798317a70a9e8
incisuinci <incisuinci@gmail.com>
2020-11-15 03:01:53 -0500

    This is replit question 150

---
+++ src/ReplitQuestions.java
@@ -0,0 +1,17 @@
+public class ReplitQuestions {
+    public static void main(String[] args) {
+        /*This is replit 150:
+        Create a method printHollowRect that prints a 5 on 5 hollow rectangle:
+*****
+*   *
+*   *
+*   *
+*****
+
+hint:you will need to use two nested for loops for that,
+and an if that checks if its the last or first iteration of the loop
+ (so you will know whet to print "*" or " ")
+
+         */
+    }
+}
